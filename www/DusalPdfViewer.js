13bd737acf4bf5d379eec70f03cde96c7ea7a37f
Davaadorj <davaa-u@ib-system.co.jp>
2018-06-09 14:40:25 +0900

    ios fix

--- www/DusalPdfViewer.js
+++ www/DusalPdfViewer.js
@@ -3,7 +3,8 @@ var exec = require('cordova/exec');
 exports.openPdf = function(url, header,options,success, error) {
     if (device.platform == "iOS") {
         window.open(encodeURI(url), '_blank', 'location=yes, EnableViewPortScale=yes');
+        exec(success || null, error || null, "DusalPdfViewer", "open", [url,'application/pdf']);
     } else {
-        exec(success, error, "DusalPdfViewer", "openPdfUrl", [url,header,options]);
+        exec(success || null, error || null, "DusalPdfViewer", "openPdfUrl", [url,header,options]);
     }
 };
