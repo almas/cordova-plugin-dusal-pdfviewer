bda115a4c1d94d4f9c8f90279a5115beb8f9727d
Davaadorj <davaa-u@ib-system.co.jp>
2018-06-13 17:26:12 +0900

    iOS 2 open bug fix

--- www/DusalPdfViewer.js
+++ www/DusalPdfViewer.js
@@ -2,7 +2,6 @@ var exec = require('cordova/exec');
 
 exports.openPdf = function(url, header,options,success, error) {
     if (device.platform == "iOS") {
-        window.open(encodeURI(url), '_blank', 'location=yes, EnableViewPortScale=yes');
         exec(success || null, error || null, "DusalPdfViewer", "open", [url,'application/pdf']);
     } else {
         exec(success || null, error || null, "DusalPdfViewer", "openPdfUrl", [url,header,options]);
